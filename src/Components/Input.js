64677dc935bf9e0bd78858d2487ccdad35ff1f76
barnes-joseph <barnsjoseph05@gmail.com>
2022-01-30 15:27:43 +0000

    Modified input component

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {FormInput,FormLabel} from '../Components/Styled'
 
-const Input = ({name,type}) => {
+const Input = ({name,type,onChange,label}) => {
     return(
         <div style={{display:'flex',flexDirection:'column'}}>
-            <FormLabel htmlFor={name}>{name}</FormLabel>
-            <FormInput name={name} type={type}/>
+            <FormLabel htmlFor={name}>{label}</FormLabel>
+            <FormInput name={name} type={type} onChange={onChange} required/>
         </div>
     )
 }
