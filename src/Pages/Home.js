84aa84c0c3fc67345c9b82f9643d9ba0778c2f66
barnes-joseph <barnsjoseph05@gmail.com>
2022-01-30 15:28:51 +0000

    Added Landing page and its components

---
+++ src/Pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import HomeNav from '../Components/HomeNav'
+import {HomeContainer,MakeNote,HomeContent,Svg,First,Second} from '../Components/Styled'
+import MemoNoteSvg from '../Components/svg'
+
+const Home = () => {
+    return (
+      <HomeContainer>
+        <HomeNav />
+        <HomeContent>
+          <MakeNote>
+            <First>
+              Make <span style={{'color':'green'}}>your</span>
+            </First>
+            <Second>notes cool</Second>
+          </MakeNote>
+          <Svg>
+            <MemoNoteSvg style={{ "font-size": "30em" }} />
+          </Svg>
+        </HomeContent>
+      </HomeContainer>
+    );
+}
+
+export default Home
