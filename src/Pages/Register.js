eb0f15abc6837a0bf5b98880ba8c2b9afa2fc07c
barnes-joseph <barnsjoseph05@gmail.com>
2022-01-30 15:27:09 +0000

    Modified login and register pages

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -2,24 +2,87 @@ import FormHeader from "../Components/FormHeader";
 import Input from "../Components/Input";
 import Logo from '../Components/Logo'
 import { FormContainer,Form,SubmitDiv,Submit, InputDiv } from "../Components/Styled";
+import {useState,useContext} from 'react'
+import { RegisterAccount,SignIn } from "../API/account-api";
+import {Auth} from "../Components/Auth"
+import {Navigate,Link} from 'react-router-dom'
 
 const Register = () => {
+  const [details,setDetails] = useState({'email':'','password':'','name':'','account_type':'normal','confirm password':''})
+  const {signIn} = useContext(Auth)
+  const [registered,setRegistered] = useState(false)
+  const [loading,setLoading] = useState(false)
+
+  const onChange = (e) =>{
+    const {name,value} = e.target
+    setDetails(prevDetails=>{
+      return {...prevDetails,[name]:value}
+    })
+  }
+
+    const Login = () => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    SignIn(headers, details, signIn);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+    const headers = new Headers()
+    headers.append('Content-Type','application/json')
+    if(details.password === details["confirm password"]){
+      RegisterAccount(headers,details)
+      Login()
+      setRegistered(true)
+      setLoading(false)
+    }
+    else{
+      alert('Passwords do not match')
+    }
+  }
+
     return (
+      !registered ?
       <FormContainer>
         <Logo />
-        <Form>
-          <FormHeader header="Login" />
+        <Form onSubmit={onSubmit}>
+          <FormHeader header="Sign Up" />
           <InputDiv>
-            <Input name="email" type="email" />
-            <Input name="password" type="password" />
-            <Input name="confirm password" type="password" />
+            <Input
+              name="name"
+              type="text"
+              onChange={onChange}
+              label='Name'
+            />
+            <Input
+              name="email"
+              type="email"
+              label='Email'
+              onChange={onChange}
+            />
+            <Input
+              name="password"
+              type="password"
+              label='Password'
+              onChange={onChange}
+            />
+            <Input
+              name="confirm password"
+              type="password"
+              label='Confirm Password'
+              onChange={onChange}
+            />
           </InputDiv>
           <SubmitDiv>
+            <Link to='/login' style={{color:'white',textDecoration:'none'}}>
             <span>Already have an account?</span>
-            <Submit type="submit">submit</Submit>
+            </Link>
+            <Submit type="submit" value={loading ? 'submitting ...' : 'submit'} />
           </SubmitDiv>
         </Form>
-      </FormContainer>
+      </FormContainer> :
+      <Navigate to='/notes'/>
     );
 }
 export default Register;
